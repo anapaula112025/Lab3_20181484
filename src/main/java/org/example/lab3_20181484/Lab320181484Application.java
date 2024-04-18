e9fbb7e370d7d6ae0fed6edaad7c943b68cd9cd4
Ana <anapaula.hachi@hotmail.com>
2024-04-17 19:11:06 -0500

    first commit

---
+++ src/main/java/org/example/lab3_20181484/Lab320181484Application.java
@@ -0,0 +1,13 @@
+package org.example.lab3_20181484;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320181484Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320181484Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab3_20181484/Lab320181484ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab3_20181484;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320181484ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
